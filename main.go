254b59c1ecdfecc3525d4a1700c1aed44e8a37e4
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-03-02 19:47:01 +0100

    new conn.read

--- main.go
+++ main.go
@@ -10,10 +10,13 @@ import (
 const (
 	verbose       bool   = false
 	netType       string = "tcp"
-	address       string = "10.28.114.89:4600" // 10.28.114.89
+	address       string = "127.0.0.1:4600" // 10.28.114.89
 	MaxBufferSize int    = 2048
 	headerSize    int    = 40
 )
+
+var buffer []byte = make([]byte, MaxBufferSize) // buffer to hold incoming data
+var allHexBytes []byte = make([]byte, MaxBufferSize)
 
 func main() {
 	listener, err := net.Listen(netType, address) // listen on port 4600
@@ -54,37 +57,38 @@ func handleConnection(conn net.Conn) {
 
 	fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-	buffer := make([]byte, MaxBufferSize) // buffer to hold incoming data
-
-	for {
-		n, err := conn.Read(buffer) // read data from the connection
+	bytesRead := 0
+	for bytesRead <= headerSize {
+		n, err := conn.Read(buffer[bytesRead:]) // read data from the connection
 		if err != nil {
 			fmt.Println("[WARNING] Error reading or Client disconnected:", err)
 			break
 		}
+		bytesRead += n
+	}
 
-		hexData := hex.EncodeToString(buffer[:n]) // convert binary data to hexadecimal representation
+	allHexBytes = buffer[:bytesRead]
 
-		hexBytes, err := hex.DecodeString(hexData)
+	hexBytesHeader := allHexBytes[:headerSize]         // Extract first 40 bytes
+	headerValues := decodeHeaderUint32(hexBytesHeader) // decode little-endian uint16 values
+	fmt.Println(">> Decoded Header values:", headerValues)
+
+	hexBytesBody := allHexBytes[headerSize:] // Extract the rest of the bytes
+
+	if verbose {
+		hexData := hex.EncodeToString(buffer[:bytesRead]) // convert binary data to hexadecimal representation
+		hexBytes, err := hex.DecodeString(hexData)        // back to bytes
 		if err != nil {
 			fmt.Println("[WARNING] Error decoding hex string:", err)
 			return
 		}
 
-		hexBytesHeader := hexBytes[:headerSize] // Extract first 40 bytes
-		hexBytesBody := hexBytes[headerSize:]   // Extract the rest of the bytes
-
-		if verbose {
-			fmt.Printf(">> Received data (hex): %s\n", hexData)      // print the received data in hexadecimal format
-			fmt.Printf("- Number of characters: %d\n", len(hexData)) // count characters
-			fmt.Printf("- Number of bytes: %d\n", len(hexBytes))     // count bytes
-		}
-
-		headerValues := decodeHeaderUint32(hexBytesHeader) // decode little-endian uint16 values
-		fmt.Println(">> Decoded Header values:", headerValues)
-
-		go handleAnswer(conn, headerValues, hexBytesBody)
+		fmt.Printf(">> Received data (hex): %s\n", hexData)      // print the received data in hexadecimal format
+		fmt.Printf("- Number of characters: %d\n", len(hexData)) // count characters
+		fmt.Printf("- Number of bytes: %d\n", len(hexBytes))     // count bytes
 	}
+
+	go handleAnswer(conn, headerValues, hexBytesBody)
 }
 
 func handleAnswer(conn net.Conn, _headerValues []uint32, _hexBytesBody []byte) {
@@ -103,8 +107,9 @@ func handleAnswer(conn net.Conn, _headerValues []uint32, _hexBytesBody []byte) {
 	case 4702: // process message
 		bodyValuesStatic, bodyValueDynamic := decodeBody(_hexBytesBody, messageType)
 		fmt.Println(">> Decoded Body values:", bodyValuesStatic, bodyValueDynamic)
-		response = encodeProcess(processType(messageTypeAns, messageCounter, bodyValuesStatic, bodyValueDynamic))
-		echo = false
+		// response = encodeProcess(processType(messageTypeAns, messageCounter, bodyValuesStatic, bodyValueDynamic))
+		response = []byte("dur process")
+		echo = true
 
 	case 4703: // acknowledge data message
 		fmt.Println("MES received data properly")
