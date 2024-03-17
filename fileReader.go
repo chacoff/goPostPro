6547362cdd58a743b7c02429dfb37a2bf67bf86a
chacoff <jaime.arctico@gmail.com>
2024-03-17 23:20:42 +0100

    dataframe, all in strings ...

--- fileReader.go
+++ fileReader.go
@@ -8,17 +8,13 @@ import (
 	"bufio"
 	"fmt"
 	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
 	"log"
 	"os"
 	"strconv"
 	"strings"
 	"time"
 )
-
-type dataFrame struct {
-	TimeStamp string // pay attention to define dataFrame struct with exported fields, capital letter first!!
-	DataTemps string
-}
 
 func main() {
 	_file, err := os.Open("D:\\00_Dev\\RD_AM\\PostProTrain2\\testing_data\\DUO01-02_0891_half.txt")
@@ -32,28 +28,52 @@ func main() {
 
 	df := createDataFrame(scanner)
 	fmt.Println(df)
-
 }
 
 func createDataFrame(scanner *bufio.Scanner) dataframe.DataFrame {
-	var datas []dataFrame
+	var stamps []string
+	var temps [][]float64
+	var _stamps string
+	var _dataLine []string
+	var _dataLineConcat string
 
 	for scanner.Scan() {
 		_line := strings.ReplaceAll(scanner.Text(), ",", ".") // Replace commas with periods, sometimes!
 		line := strings.Fields(_line)                         // like str.join.(' ')
-		_dataLine := line[7:10]                               // extract temperature data, elements from 7 to (total-4)
-		_dataLineConcat := strings.Join(_dataLine, " ")       // because dataframe.LoadStructs doesn't support []string []float64
 
-		_df := dataFrame{
-			line[0] + " " + line[1], // extract timestamp, elements 0 and 1
-			_dataLineConcat,
-		}
-		datas = append(datas, _df)
+		_stamps = line[0] + " " + line[1]
+		_dataLine = line[7 : len(line)-4] // extract temperature data, elements from 7 to (total-4)
+
+		_dataLineConcat = strings.Join(_dataLine, " ")
+		_tempsArray := dataFormatter(_dataLineConcat)
+		stamps = append(stamps, _stamps)
+		temps = append(temps, _tempsArray)
 	}
 
-	df := dataframe.LoadStructs(datas)
+	tempsStrings := arrayFloatToString(temps)
+
+	df := dataframe.New(
+		series.New(stamps, series.String, "TimeStamps"),
+		series.New(tempsStrings, series.String, "Temperatures"),
+	)
 
 	return df
+}
+
+func arrayFloatToString(temps [][]float64) []string {
+	var tempsStrings []string
+	for _, t := range temps {
+		tempsStrings = append(tempsStrings, arrayToString(t))
+	}
+	return tempsStrings
+}
+
+func arrayToString(arr []float64) string {
+	strArr := make([]string, len(arr))
+	for i, v := range arr {
+		strArr[i] = fmt.Sprintf("%f", v)
+	}
+	return strings.Join(strArr, " ")
 }
 
 func timeFormatter(s string) time.Time {
