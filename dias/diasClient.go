6a939b613138968b477dec68b92cadb95dfb2d2d
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-03-26 19:04:15 +0100

    first version of diasService communicating without errors

--- dias/diasClient.go
+++ dias/diasClient.go
@@ -6,7 +6,7 @@ import (
 	"net"
 )
 
-func diasClient() {
+func DiasClient() {
 	addr, _ := net.ResolveTCPAddr("tcp", "127.0.0.1:5603")
 	conn, err := net.DialTCP("tcp", nil, addr)
 	if err != nil {
--- dias/diasServer.go
+++ dias/diasServer.go
@@ -1,4 +1,4 @@
-package main
+package dias
 
 import (
 	"encoding/binary"
@@ -8,22 +8,21 @@ import (
 	"net"
 )
 
-var (
-	i      int = 0
-	buffer     = make([]byte, 2048)
-)
+var buffer = make([]byte, 24)
 
-func main() {
+func DiasServer() {
 	ln, err := net.Listen("tcp", "127.0.0.1:2002")
 	if err != nil {
-		fmt.Println("problems listening")
+		log.Fatal("problems listening: ", err)
 	}
+	defer ln.Close()
 	fmt.Println("Listen on port: 127.0.0.1:2002")
 
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Println("error accepting connection: ", err)
+			continue
 		}
 		fmt.Println("Accepted connection on port")
 		go handleDiasConnection(conn)
@@ -31,40 +30,34 @@ func main() {
 }
 
 func handleDiasConnection(conn net.Conn) {
-	defer func(conn net.Conn) {
-		err := conn.Close()
-		if err != nil {
-			fmt.Println("disconnection ...")
-		}
-	}(conn)
 
-	_buffer := make([]byte, 0, 2048)
 	for {
-		fmt.Println(":Debug", i)
-		n, err := conn.Read(buffer) // read data from the connection
+		n, err := conn.Read(buffer)
 		if err != nil {
-			fmt.Println("error reading or disconnection")
+			fmt.Println("error reading from connection: ", err)
 			break
 		}
 
 		message := hex.EncodeToString(buffer[:n])
-		fmt.Println("Message Received: ", message, len(buffer[:n]))
+		fmt.Println("Message Received: ", message)
 
-		values := []uint16{1500, 600, 700, 800, 600, 750, 850, 1160}
+		answer := make([]byte, 0)
+		values := []uint16{1501, 605, 706, 808, 609, 753, 855, 1165}
 		for _, val := range values {
 			binaryValue := make([]byte, 2)
 			binary.LittleEndian.PutUint16(binaryValue, val)
-			_buffer = append(_buffer, binaryValue...) // slice, element to unpack
+			answer = append(answer, binaryValue...)
 		}
-		i += 1
 
+		_, err = conn.Write(answer)
+		if err != nil {
+			fmt.Println("error writing response: ", err)
+			break
+		} else {
+			_length := len(answer)
+			fmt.Printf("Sent %q with length: %d\n", hex.EncodeToString(answer), _length)
+		}
 	}
-	go handleAnswer(conn, _buffer)
-}
 
-func handleAnswer(conn net.Conn, _buffer []byte) {
-	fmt.Println("length answer: ", len(_buffer))
-	fmt.Println(hex.EncodeToString(_buffer))
-	conn.Write(_buffer)
-	fmt.Println("Sent.")
+	conn.Close()
 }
