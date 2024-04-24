57dcb14f2ae77545c7da3f0122e246fda5c160fe
chacoff <jaime.arctico@gmail.com>
2024-04-24 23:22:30 +0200

    added default for LTCFromMes

--- global/global.go
+++ global/global.go
@@ -6,4 +6,4 @@ import (
 
 var Appconfig config.Config = config.LoadConfig()
 
-var LTCFromMes []uint16 // TODO only a workaround!
+var LTCFromMes []uint16 = []uint16{1, 2, 3, 4, 5, 6, 7, 8} // TODO only a workaround!
