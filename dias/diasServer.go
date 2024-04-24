68c0d68535409fed2a52daaee3434b9862c9707a
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-04-24 17:31:26 +0200

    wip dias data pointer

--- dias/diasServer.go
+++ dias/diasServer.go
@@ -15,6 +15,8 @@ var buffer = make([]byte, 24)
 // DiasServer opens socket communication with DIAS software. Objective is to pass the LTC value
 func DiasServer(valuesToDias <-chan []uint16) {
 
+	var LTCValues []uint16
+
 	ln, err := net.Listen(global.Appconfig.NetType, global.Appconfig.AddressDias)
 	if err != nil {
 		log.Fatal("problems listening: ", err)
@@ -30,12 +32,19 @@ func DiasServer(valuesToDias <-chan []uint16) {
 		}
 		fmt.Println("Accepted DIAS-client")
 
-		data := <-valuesToDias
-		go handleDiasConnection(conn, data)
+		go handleDiasConnection(conn, LTCValues)
+
+		select {
+		case LTCValues = <-valuesToDias:
+			fmt.Println("DIAS: Received data from channel:", LTCValues) // Data is available on the channel, process it
+		default:
+			LTCValues = []uint16{1,2,3,4,5,6,7,8} // dummy LTCs if the channel is empty
+			fmt.Println("DIAS: No data available on the channel. Using defaults: ", LTCValues)  // No data available on the channel, do something else
+		}
 	}
 }
 
-func handleDiasConnection(conn net.Conn, values []uint16) {
+func handleDiasConnection(conn net.Conn, LTCValues []uint16) {
 
 	for {
 		n, err := conn.Read(buffer)
@@ -48,10 +57,10 @@ func handleDiasConnection(conn net.Conn, values []uint16) {
 		if global.Appconfig.Verbose {
 			fmt.Println("Message Received from Dias: ", message)
 		}
-		fmt.Println("Updating Dias:", values)
+		fmt.Println("Updating Dias:", LTCValues)
 
 		answer := make([]byte, 0)
-		for _, val := range values {
+		for _, val := range LTCValues {
 			binaryValue := make([]byte, 2)
 			binary.LittleEndian.PutUint16(binaryValue, val)
 			answer = append(answer, binaryValue...)
