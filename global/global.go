0eed8927b64534af1fb4e0b8cee923851d2d41b1
chacoff <jaime.arctico@gmail.com>
2024-04-25 23:23:54 +0200

    correct comment standard

--- global/global.go
+++ global/global.go
@@ -4,7 +4,7 @@ import (
 	"goPostPro/config"
 )
 
-// Main Software
+// Appconfig Main Software
 var Appconfig config.Config = config.LoadConfig()
 
 var LTCFromMes []uint16 = []uint16{1, 2, 3, 4, 5, 6, 7, 8} // TODO only a workaround!
