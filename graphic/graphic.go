5dfee3a5dae702d02aa67ac4267ef20993afe892
Theo.Verbrugge <Theo.Verbrugge@arcelormittal.com>
2024-05-15 15:42:33 +0200

    Comments
    (cherry picked from commit ea1206d6f6dbd8732c4d1701d2c6296ca0494ec2)

--- graphic/graphic.go
+++ graphic/graphic.go
@@ -1,3 +1,14 @@
+/*
+ * File:    graphic.go.go
+ * Date:    May 15, 2024
+ * Author:  T.V
+ * Email:   theo.verbrugge77@gmail.com
+ * Project: goPostPro
+ * Description:
+ *   Contains some functions to display an image of the processing
+ *
+ */
+
 package graphic
 
 import (
@@ -12,25 +23,28 @@ import (
 	"github.com/mazznoer/colorgrad"
 )
 
+// Variables used to write correctly in the global image
 var recording_image *image.RGBA
 var image_line int = 0
 var first_timestamp time.Time = time.Now()
 var offset int = 0
 
+// Used to convert a temperature into a thermal color
 func thermalColor(temperature float64) color.Color {
 	domain_value := (temperature - float64(global.Graphics.ThermalScaleStart)) / (float64(global.Graphics.ThermalScaleEnd) - float64(global.Graphics.ThermalScaleStart))
 	return colorgrad.Inferno().At(domain_value)
 }
 
+// Save the global image with the timestamps of beginning and end of measurement
 func saveImage() error {
+	//Create the file
 	filename := first_timestamp.Format("15-04-05") + "_" + time.Now().Format("15-04-05") + ".png"
 	imageFile, creation_error := os.Create(filename)
 	if creation_error != nil {
 		return creation_error
 	}
 	defer imageFile.Close()
-
-	// Écrire l'image dans le fichier PNG
+	//Write our image in the file
 	encoding_error := png.Encode(imageFile, recording_image)
 	if encoding_error != nil {
 		return encoding_error
@@ -38,6 +52,7 @@ func saveImage() error {
 	return nil
 }
 
+// Create a new image by reseting the variables used
 func NewImage() error {
 	log.Println(global.Graphics.ImageWidth, global.Graphics.ImageHeight)
 	recording_image = image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{global.Graphics.ImageWidth, global.Graphics.ImageHeight}})
@@ -46,7 +61,7 @@ func NewImage() error {
 	return nil
 }
 
-// Go to the next line of the image or create a new image
+// Go to the next line of the image or create a new image if we reached the bottom of the picture
 func NewLine() error {
 	image_line++
 	if image_line > global.Graphics.ImageHeight {
@@ -62,6 +77,7 @@ func NewLine() error {
 	return nil
 }
 
+// Draw the left part which is the original thermal image
 func DrawBeforeProcessing(temperature_array []float64) error {
 	for index := 0; index < len(temperature_array); index++ {
 		recording_image.Set(index, image_line, thermalColor(temperature_array[index]))
@@ -69,6 +85,7 @@ func DrawBeforeProcessing(temperature_array []float64) error {
 	return nil
 }
 
+// Draw the right part which is the image after thresholding
 func DrawAfterProcessing(processed_temperature_array []float64) error {
 	for index := 0; index < len(processed_temperature_array); index++ {
 		recording_image.Set(global.Graphics.ImageWidth/2+index, image_line, thermalColor(processed_temperature_array[index]))
@@ -76,6 +93,7 @@ func DrawAfterProcessing(processed_temperature_array []float64) error {
 	return nil
 }
 
+// Draw the borders of the detected product
 func DrawBorders(left_index int, right_index int) error {
 	offset = global.Graphics.ImageWidth/2 + left_index
 	recording_image.Set(offset, image_line, color.RGBA{0, 255, 0, 255})
@@ -83,6 +101,7 @@ func DrawBorders(left_index int, right_index int) error {
 	return nil
 }
 
+// Draw the limits used (max of each side) for the web
 func DrawRegions(max_tr1 int, max_tr3 int) error {
 	recording_image.Set(offset+max_tr1, image_line, color.RGBA{0, 255, 255, 255})
 	recording_image.Set(offset+max_tr3, image_line, color.RGBA{0, 255, 255, 255})
