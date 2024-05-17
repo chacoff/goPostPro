e50e2a99380f1f3a63579e12d97c0235025dc19d
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-05-17 11:40:20 +0200

    initial commit

--- _ExternalLibs/WindowsService/uninstall.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem run this script as admin
-
-net stop goPostProTr2
-sc delete goPostProTr2
--- _Resources/launchgoPostPro.c
+++
@@ -1,9 +0,0 @@
-//go:build ignore
-#include <stdlib.h>
-
-int main() {
-    // Replace Trayrunner.exe with argument goPostPro
-    system("start /B Trayrunner.exe goPostPro > nul 2>&1");
-
-    return 0;
-}
