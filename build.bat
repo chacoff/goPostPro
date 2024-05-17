c3ab0d88b45bb03e542fb3492a3e0732906f2d90
Jaime Gomez-Chacoff <jaime.gomez-chacoff@arcelormittal.com>
2024-05-17 14:53:09 +0200

    global buildVersion

--- build.bat
+++ build.bat
@@ -15,37 +15,52 @@ rem Set variables -----------------------------------------------------------
 set target_folder=./BuildMachine/release
 set config_file=config.xml
 set previous_builds_folder=./BuildMachine/previousReleases
-set counter_file=./_Resources/buildVersion.txt
+set counter_file=buildVersion.txt
 set icon=./_Resources/beam.ico
+
+rem Taking the remote buildVersion number -----------------------------------
+git fetch
+
+git checkout buildVersion
+
+git pull
+
+    rem Generate a build number ---------------------------------------------
+    if not exist "%counter_file%" (
+      echo 1 > "%counter_file%"
+    )
+    
+    for /f "usebackq tokens=*" %%a in ("%counter_file%") do set patch_version=%%a
+    
+        set previousBuild=%patch_version%
+    
+        set previousMajor=!previousBuild:~0,1!
+        set previousMinor=!previousBuild:~1,1!
+        set previousPatch=!previousBuild:~2!
+        echo Previous Build Number: %previousMajor%.%previousMinor%.%previousPatch%
+        
+        set /a patch_version=%patch_version%+1
+        echo %patch_version% > "%counter_file%"
+        
+        set number=%patch_version%
+        set major=!number:~0,1!
+        set minor=!number:~1,1!
+        set patch=!number:~2!
+        echo Current Build Number: %major%.%minor%.%patch%
+        
+
+        rem set build number Variables --------------------------------------
+        set previousBuildNumber=%previousMajor%.%previousMinor%.%previousPatch%
+        set buildNumber=%major%.%minor%.%patch%
+
+git commit -a -m 'updated build version'
+
+git push -u origin buildVersion
+
+git checkout dev
 
 rem Create target folder if it doesn't exist --------------------------------
 if not exist "%previous_builds_folder%" mkdir "%previous_builds_folder%"
-
-rem Generate a build number -------------------------------------------------
-if not exist "%counter_file%" (
-  echo 1 > "%counter_file%"
-)
-
-for /f "usebackq tokens=*" %%a in ("%counter_file%") do set patch_version=%%a
-
-    set previousBuild=%patch_version%
-
-    set previousMajor=!previousBuild:~0,1!
-    set previousMinor=!previousBuild:~1,1!
-    set previousPatch=!previousBuild:~2!
-    echo Previous Build Number: %previousMajor%.%previousMinor%.%previousPatch%
-    set previousBuildNumber=%previousMajor%.%previousMinor%.%previousPatch%
-
-    set /a patch_version=%patch_version%+1
-    echo %patch_version% > "%counter_file%"
-    
-    set number=%patch_version%
-    set major=!number:~0,1!
-    set minor=!number:~1,1!
-    set patch=!number:~2!
-    echo Current Build Number: %major%.%minor%.%patch%
-    set buildNumber=%major%.%minor%.%patch%
-
 
 rem Store previous releases -------------------------------------------------
 if exist "%target_folder%-%previousBuildNumber%" (
