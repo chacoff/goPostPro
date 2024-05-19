0559240e9090211b95f070906af1dc5bbd7df2de
chacoff <jaime.arctico@gmail.com>
2024-05-19 14:11:51 +0200

    apps to launch are now internal parameters

--- _Resources/launchgoPostPro.c
+++ _Resources/launchgoPostPro.c
@@ -1,8 +1,11 @@
 //go:build ignore
 #include <stdlib.h>
+#define APP "Trayrunner.exe "   // app to launch
+#define ARGS "goPostPro "       // arguments
 
 int main() {
-    char command[] = "start /B Trayrunner.exe goPostPro > nul 2>&1""start /B Trayrunner.exe goPostPro > nul 2>&1"
+    char command[] = "start /B " APP ARGS "> nul 2>&1";
+    // printf("%s", command);
     system(command);
 
     return 0;
