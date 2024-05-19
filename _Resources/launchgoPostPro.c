bcc4643ed140a24851cc1cb9e0a4880da7aa76a0
chacoff <jaime.arctico@gmail.com>
2024-05-19 13:53:39 +0200

    updated build version

--- _Resources/launchgoPostPro.c
+++ _Resources/launchgoPostPro.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 
 int main() {
-    // Replace Trayrunner.exe with argument goPostPro
-    system("start /B Trayrunner.exe goPostPro > nul 2>&1");
+    char command[] = "start /B Trayrunner.exe goPostPro > nul 2>&1""start /B Trayrunner.exe goPostPro > nul 2>&1"
+    system(command);
 
     return 0;
 }
