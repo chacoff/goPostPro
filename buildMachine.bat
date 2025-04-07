9a0b5ac8dbe29fd96039d216d24ddfa0f7abe7e7
chacoff <jaime.arctico@gmail.com>
2025-04-07 18:59:33 +0200

    build machine including NSSM

--- buildMachine.bat
+++ buildMachine.bat
@@ -2,63 +2,26 @@
 setlocal
 setlocal enabledelayedexpansion
 
-rem Check if parameters are provided ----------------------------------------
-if "%~1" == "" (
-    echo Usage: build.bat [executable_name]
-    echo Using default: goPostPro.exe
-    set executable_name=goPostPro.exe
-) else (
-    set executable_name=%~1
-)
+set previousBuildNumber=1.0.059
+set number=99995
 
-rem Set variables -----------------------------------------------------------
+set major=!number:~0,1!
+set minor=!number:~1,1!
+set patch=!number:~2!
+set patchv=!number:~2,1!
+set patch2=!number:~3!
+
+set buildNumber=%major%.%minor%.%patch%
+set buildNumber2=%major%,%minor%,%patchv%,%patch2%
+
 set target_folder=./BuildMachine/release
-set config_file=config.xml
 set previous_builds_folder=./BuildMachine/previousReleases
-set counter_file=R:/gopostpro/buildVersion.txt
-set icon=./_Resources/beam.ico
-
-rem Taking the remote buildVersion number -----------------------------------
-
-    rem Generate a build number ---------------------------------------------
-    if not exist "%counter_file%" (
-      echo 10000 > "%counter_file%"
-    )
-    
-    for /f "usebackq tokens=*" %%a in ("%counter_file%") do set patch_version=%%a
-    
-        set previousBuild=%patch_version%
-    
-        set previousMajor=!previousBuild:~0,1!
-        set previousMinor=!previousBuild:~1,1!
-        set previousPatch=!previousBuild:~2!
-        echo Previous Build Number: %previousMajor%.%previousMinor%.%previousPatch%
-        
-        set /a patch_version=%patch_version%+1
-        echo %patch_version% > "%counter_file%"
-        
-        set number=%patch_version%
-        set major=!number:~0,1!
-        set minor=!number:~1,1!
-        set patch=!number:~2!
-        set patchv=!number:~2,1!
-        set patch2=!number:~3!
-        echo Current Build Number: %major%.%minor%.%patch%
-        
-
-        rem set build number Variables --------------------------------------
-        set previousBuildNumber=%previousMajor%.%previousMinor%.%previousPatch%
-        set buildNumber=%major%.%minor%.%patch%
-        set buildNumber2=%major%,%minor%,%patchv%,%patch2%
-
-echo Push to origin.buildVersion successful!
-
-timeout /t 1
+set executable_name=goPostPro.exe
+set config_file=config.xml
 
 rem copy version.info.rc --------------------------------
 echo Preparing versioninfo and FART
 copy "_Resources\versioninfo.rc" "versioninfo.rc"
-copy "_Resources\versionlauncher.rc" "versionlauncher.rc"
 copy "_Resources\fart.exe" "fart.exe"
 copy "_Resources\beam.ico" "beam.ico"
 copy "_Resources\golang.ico" "golang.ico"
@@ -67,7 +30,6 @@ copy "_Resources\Poppins-SemiBold.ttf" "Poppins-SemiBold.ttf"
 rem Update and Generate versioninfo.rc, versioninfo.syso --------------------------------
 echo Version: %buildNumber2%
 .\fart.exe versioninfo.rc "0,0,0,1" %buildNumber2%
-.\fart.exe versionlauncher.rc "0,0,0,1" %buildNumber2%
 
 rem Build versioninfo.syso --------------------------------
 windres -i versioninfo.rc -O coff -o versioninfo.syso
@@ -87,7 +49,7 @@ go build -ldflags "-s -w" -o "%target_folder%-%buildNumber%\%executable_name%"
 
 rem Copy the config file and external libs to complete the release -----------
 copy "config\%config_file%" "%target_folder%-%buildNumber%\%config_file%"
-copy "_ExternalLibs\TrayRunner\*.*" "%target_folder%-%buildNumber%"
+copy "_ExternalLibs\WindowsService\nssm.exe" "%target_folder%-%buildNumber%"
 copy "_Resources\beam.ico" "%target_folder%-%buildNumber%\beam.ico"
 copy "_Resources\golang.ico" "%target_folder%-%buildNumber%\golang.ico"
 copy "_Resources\Poppins-SemiBold.ttf" "%target_folder%-%buildNumber%\Poppins-SemiBold.ttf"
@@ -96,21 +58,15 @@ rem update config XML using xmlstarlet --------------------------------------
 xmlstarlet ed -L -u /parameters/build/version -v %buildNumber% %target_folder%-%buildNumber%/%config_file%
 echo XML update completed with build number: %buildNumber%
 
-rem Create launcher and syso file for launcher ------------------------------
-rem windres _Resources\icon.rc -O coff -o _Resources\icon.o
-windres -i versionlauncher.rc -O coff -o versionlauncher.syso
-gcc _Resources\launchgoPostPro.c versionlauncher.syso -o %target_folder%-%buildNumber%\LaunchGoPostPro.exe
-echo Shortcut created successfully.
-
 rem Cleaning -----------------------------------------------------------------
 del /F "versioninfo.rc"
 del /F "versioninfo.syso"
-del /F "versionlauncher.rc"
-del /F "versionlauncher.syso"
 del /F "fart.exe"
 del /F "beam.ico"
 del /F "golang.ico"
 del /F "Poppins-SemiBold.ttf"
+del /F "%target_folder%-%buildNumber%\processed.db"
+del /F "%target_folder%-%buildNumber%\logs\livePostPro.log"
 
 echo Build completed for release-%buildNumber%.
 
